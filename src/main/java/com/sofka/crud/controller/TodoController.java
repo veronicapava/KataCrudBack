0f01f11c7abe93a8286443c8d7ba4384eb8d9eb3
veronicapava <veronicapava@gmail.com>
2022-02-22 22:42:25 -0500

    Arreglo de rutas

--- src/main/java/com/sofka/crud/controller/TodoController.java
+++ src/main/java/com/sofka/crud/controller/TodoController.java
@@ -4,38 +4,38 @@ import com.sofka.crud.model.Todo;
 import com.sofka.crud.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
 public class TodoController {
 
     @Autowired
     private TodoService service;
 
-    @GetMapping(value="api/todos")
+    @GetMapping(value="/todos")
     public Iterable<Todo> list(){ //Para listar todos los elementos
         return service.list();
     }
 
-    @PostMapping(value = "api/todo")
+    @PostMapping(value = "/todo")
     public Todo save(@RequestBody Todo todo){ //Para guardar los elementos
         return service.save(todo);
     }
 
-    @PutMapping(value="api/todo")
+    @PutMapping(value="/todo")
     public Todo update(@RequestBody Todo todo){ //Para actualizar un elemento
         if(todo.getId() != null){
             return service.save(todo);
-        } throw new RuntimeException("NO existe el id para actualizar");
+        } throw new RuntimeException("No existe el id para actualizar");
     }
 
-    @DeleteMapping("api/{id}/todo")
+    @DeleteMapping("/todo/{id}")
     public void delete(@PathVariable("id") Long id){ //Para eliminar un elemento
         service.delete(id);
     }
 
-    @GetMapping(value="api/{id}/todo")
+    @GetMapping(value="/todo/{id}")
     public Todo get(@PathVariable("id") Long id){ //Para buscar por id
         return service.get(id);
     }
--- src/main/java/com/sofka/crud/service/TodoService.java
+++ src/main/java/com/sofka/crud/service/TodoService.java
@@ -5,7 +5,6 @@ import com.sofka.crud.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class TodoService {
